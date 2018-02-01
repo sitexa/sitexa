4758c8a9a0cf2df1c3d9abb06409bc1d4d49396a
xnpeng <xnpeng@hotmail.com>
2018-02-01 15:59:13 +0800

    reboot

--- src/com/sitexa/ktor/HelloWorld.kt
+++ src/com/sitexa/ktor/HelloWorld.kt
@@ -5,6 +5,30 @@ package com.sitexa.ktor
  *
  */
 
-fun main(vararg:Array<String>){
+fun main(vararg: Array<String>) {
     println("Hello world!")
+    xyz()
+}
+
+fun xyz() {
+    val num = 1000
+    val result = 4f
+
+    for (x in 1..num) {
+        for (y in 1..num) {
+            for (z in 1..num) {
+                val a = x.toFloat() / (y.toFloat() + z.toFloat())
+                val b = y.toFloat() / (z.toFloat() + x.toFloat())
+                val c = z.toFloat() / (x.toFloat() + y.toFloat())
+
+                val r = a + b + c
+
+                if (result == r) {
+                    println("x=$x;y=$y;z=$z")
+                    println("a=$a;b=$b;c=$c")
+                    println("===============")
+                }
+            }
+        }
+    }
 }
